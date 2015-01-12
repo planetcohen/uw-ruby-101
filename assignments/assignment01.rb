e1c01f03ed3aa71daf4e451aeee5f8ff0c775fb1
Gordon Turibamwe <turibamwe@outlook.com>
2015-01-11 22:45:48 -0500

    Update assignment01.rb
    First assignment done

--- assignments/assignment01.rb
+++ assignments/assignment01.rb
@@ -1,4 +1,6 @@
 # ========================================================================================
+#  Assignment solutions submitted by Gordon Turibamwe
+#
 #  Sample Problem - `number_to_string`
 def number_to_string(n, lang)
   lang_digit_strings = {
@@ -38,7 +40,25 @@ end
 # it accepts a string
 # and returns the same string with each word capitalized.
 def titleize(s)
-  # your implementation here
+
+  # => PSEUDO CODE
+  # => -----------------------------------------------------------------
+  # => downcase all characters, then capitalize first character
+  # => iterate the string, capitalize each first letter every after space
+  # => save each change to new string
+  # => return the string
+  # => ------------------------------------------------------------------
+
+  s = s.downcase.capitalize
+  count = 0
+  while count < s.length
+    if s[count] == " "
+      s[count + 1] = s[count + 1].capitalize
+    end
+    count += 1
+  end
+  print s
+  return s
 end
 
 # Your method should generate the following results:
@@ -53,7 +73,23 @@ titleize "gooDbye CRUel wORLD"  #=> "Goodbye Cruel World"
 # Write your own implementation of `reverse` called `my_reverse`
 # You may *not* use the built-in `reverse` method
 def my_reverse(s)
-  # your implementation here
+
+  # => PSEUDO CODE
+  # => -----------------------------------------------------------------
+  # => count the length of the string use it as the countdown
+  # => iterate string to the last character - zero index
+  # => save each letter to a newString
+  # => return the new string
+  # => ------------------------------------------------------------------
+
+  newString = ""  # => initialize new string
+  count = s.length - 1 # => counting down
+  while count >= 0
+    newString += s[count]
+    count -= 1
+  end
+  print newString
+  return newString
 end
 
 # Your method should generate the following results:
@@ -67,8 +103,33 @@ my_reverse "Goodbye Cruel World"  #=> "dlroW leurC eybdooG"
 
 # Write a method `palindrome?`
 # that determines whether a string is a palindrome
+  
 def palindrome?(s)
-  # your implementation here
+  
+  # => PSEUDO CODE
+  # => -----------------------------------------------------------------
+  # => count the length of the string use it as the countdown
+  # => iterate string to the last character - zero index
+  # => compare last char and first char if the same then go to the nex if not the exit
+  # => return true of false
+  # => ------------------------------------------------------------------
+  
+  firstChar = 0
+  lastChar = s.length - 1
+  while firstChar < s.length
+    s = s.downcase
+    if s[firstChar].eql?(s[lastChar])
+      print true
+      return true
+    else
+      print false
+      return false
+    end
+
+    firstChar += 1
+    lastChar -= 1
+  end
+  
 end
 
 # Your method should generate the following results:
@@ -78,3 +139,6 @@ palindrome? "abb"                              #=> false
 
 palindrome? "Able was I ere I saw elba"        #=> true
 palindrome? "A man, a plan, a canal, Panama"   #=> true
+
+
+
