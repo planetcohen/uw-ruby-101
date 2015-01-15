1ad5c375578a36467cc1fd66406bcf4a23993d25
douglasem <douglasem@users.noreply.github.com>
2015-01-15 06:22:02 -0800

    Douglas McGowan - RUBY DL 110 - Assignment #1

--- assignments/assignment01.rb
+++ assignments/assignment01.rb
@@ -1,34 +1,6 @@
-# ========================================================================================
-#  Sample Problem - `number_to_string`
-def number_to_string(n, lang)
-  lang_digit_strings = {
-    en: %w(zero one two three four five six seven eight nine),
-    de: %w(null eins zwei drei vier fünf sechs sieben acht neun),
-    es: %w(cero uno dos tres cuatro cinco seis siete ocho nueve),
-    fr: %w(zéro un deux trois quatre cinq six sept huit neuf)
-  }
-  
-  digit_strings = lang_digit_strings[lang]
-  return "no such language" if digit_strings.nil?
-
-  s = n.to_s
-  string_digits = s.split ""
-
-
-  num_digits = []
-  string_digits.each do |s|
-    num_digits << s.to_i
-  end
-  
-  result = ""
-  num_digits.each do |digit|
-    result << digit_strings[digit]
-    result << " "
-  end
-
-  result.chop
-end
-
+# Homework #1
+# Douglas McGowan
+# RUBY DL110
 
 # ========================================================================================
 #  Problem 1 - `titleize`
@@ -38,13 +10,21 @@ end
 # it accepts a string
 # and returns the same string with each word capitalized.
 def titleize(s)
-  # your implementation here
+    num_letters = s.length	#Get number of characters in string
+	s.downcase!		#Turn all letters to lowercase
+	s.capitalize!		#Capitalize the first word in the string
+	(1...num_letters).each do |n|		#Loop to capitalize any letter after a space
+		if s[n-1].chr==" "
+			s[n]=s[n].chr.upcase
+		end	
+	end
+ return s			#Return result
 end
 
 # Your method should generate the following results:
-titleize "hEllo WORLD"          #=> "Hello World"
+#titleize "hEllo WORLD"          #=> "Hello World"
 
-titleize "gooDbye CRUel wORLD"  #=> "Goodbye Cruel World"
+#titleize "gooDbye CRUel wORLD"  #=> "Goodbye Cruel World"
 
 
 # ========================================================================================
@@ -53,13 +33,20 @@ titleize "gooDbye CRUel wORLD"  #=> "Goodbye Cruel World"
 # Write your own implementation of `reverse` called `my_reverse`
 # You may *not* use the built-in `reverse` method
 def my_reverse(s)
-  # your implementation here
+	reverse_string  = s.downcase		
+		#Purpose is to get a new string with same legth as 's'. 
+		#Using downcase avoids error (maybe a relational error that causes program to run incorrectly) 
+	num_letters = s.length
+	(0...num_letters).each do |n|		#Loop to reverse sequence of characters
+		reverse_string[n]=s[num_letters-1-n]
+	end
+  return reverse_string 	#Return result
 end
 
 # Your method should generate the following results:
-my_reverse "Hello World"          #=> "dlroW olleH"
+#my_reverse "Hello World"          #=> "dlroW olleH"
 
-my_reverse "Goodbye Cruel World"  #=> "dlroW leurC eybdooG"
+#my_reverse "Goodbye Cruel World"  #=> "dlroW leurC eybdooG"
 
 
 # ========================================================================================
@@ -68,13 +55,38 @@ my_reverse "Goodbye Cruel World"  #=> "dlroW leurC eybdooG"
 # Write a method `palindrome?`
 # that determines whether a string is a palindrome
 def palindrome?(s)
-  # your implementation here
+
+   result = false
+
+	s.delete! " "	#remove punctuation
+	s.delete! ","
+	s.delete! "."
+	s.delete! "!"  		
+	
+	if s.length%2 == 0
+		midpoint1 = s.length/2-1
+		midpoint2 = s.length/2
+	else
+		midpoint1 = (s.length-1)/2-1
+		midpoint2 = (s.length-1)/2+1
+	end
+	
+	puts word1 = s[0..midpoint1].downcase
+	puts word2 = s[midpoint2..s.length].downcase.reverse
+		
+	if word1 == word2
+	result = true
+	end
+		
+	
+   return result
+
 end
 
 # Your method should generate the following results:
-palindrome? "abba"                             #=> true
-palindrome? "aBbA"                             #=> true
-palindrome? "abb"                              #=> false
+#palindrome? "abba"                             #=> true
+#palindrome? "aBbA"                             #=> true
+#palindrome? "abb"                              #=> false
 
-palindrome? "Able was I ere I saw elba"        #=> true
-palindrome? "A man, a plan, a canal, Panama"   #=> true
+#palindrome? "Able was I ere I saw elba"        #=> true
+#palindrome? "A man, a plan, a canal, Panama"   #=> true
