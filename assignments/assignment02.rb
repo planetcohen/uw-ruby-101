28520820209363f0850cee7e72b67dd4d6e7f419
Chris Peak <christopeak@gmail.com>
2015-01-19 22:16:31 -0800

    assignment 2 tasks 1-3 complete

--- assignments/assignment02.rb
+++ assignments/assignment02.rb
@@ -11,6 +11,10 @@
 
 def to_sentence(ary)
   # your implementation here
+	str1 = ary[0...-1].reduce {|item, acc| item.to_s + ", #{acc}"}
+	a2 = [str1, ary[-1]]
+	a2.compact!
+	a2.join " and "
 end
 
 # Your method should generate the following results:
@@ -25,11 +29,17 @@ to_sentence [1, "paul", 3, "ringo"]  #=> "1, paul, 3 and ringo"
 
 # implement methods "mean", "median" on Array of numbers
 def mean(ary)
-  # your implementation here
+  tot = ary.reduce {|i, acc| i + acc }
+  avg = tot.to_f/ary.length
 end
 
 def median(ary)
-  # your implementation here
+  alen = ary.length
+  if alen % 2 == 1 then
+  	med = ary[alen/2]
+  else
+  	mean([ary[alen/2],ary[(alen/2)+1]])
+  end
 end
 
 # Your method should generate the following results:
@@ -46,6 +56,11 @@ median [1, 1, 4]  #=> 1
 # implement method `pluck` on array of hashes
 def pluck(ary, key)
   # your implementation here
+  out_array = []
+  ary.each do |h|
+  	out_array << h[key]
+  end
+  out_array
 end
 
 # Your method should generate the following results:
@@ -56,6 +71,8 @@ records = [
   {name: "Ringo",  instrument: "drums" }
 ]
 pluck records, :name        #=> ["John", "Paul", "George", "Ringo"]
+
+
 pluck records, :instrument  #=> ["guitar", "bass", "guitar", "drums"]
 
 
