0e55d19b92212af7e6fcd83e6d3b07a58c9c1791
Tim Hall <timhall3000@gmail.com>
2015-02-04 22:14:07 -0800

    Tim Hall - Assignment 4

--- assignments/assignment04.rb
+++ assignments/assignment04.rb
@@ -12,6 +12,13 @@
 # F[n] -> F[n-2] + F[n-1]
 
 def fib(n)
+  if n < 0 
+    puts "n cannot be negative."
+  elsif n == 0 || n == 1
+    return 1
+  else
+    return fib(n-1) + fib(n-2)
+  end 
   # your implementation here
 end
 
@@ -39,24 +46,70 @@ q.dequeue           #=> "second"
 q.dequeue           #=> nil
 
 class Queue
+  attr_accessor :nodes, :head, :tail
+  class Node
+    attr_accessor :item, :link
+    def initialize(item, link)
+      @item = item
+      @link = link
+    end
+  end
+
   def initialize
-    # your implementation here
+    @nodes = nil
   end
+
   def enqueue(item)
-    # your implementation here
+    n = Node.new item, @nodes
+    if self.length == 0         #mark the very first item in the queue, so it can be dequeued
+      @head = n
+    end
+    @nodes = n
   end
+
   def dequeue
-    # your implementation here
+    node = @nodes
+    result = ""
+
+    if node.link == nil       #if only one item in queue when dequeue is called, removing it causes @nodes to equal nil
+      @nodes = nil
+    end
+
+    while node
+      if node.link == @head     #dequeue head, and set head to new last element
+        node.link = nil
+        result = @head.item
+        @head = node
+      else
+        result = node.item
+      end
+      node = node.link
+    end
+    result
   end
+
   def empty?
-    # your implementation here
+    @nodes.nil?
   end
+
   def peek
-    # your implementation here
+    @nodes.nil? ? nil : @nodes.item
   end
+
   def length
-    # your implementation here
+    count = 0
+    node = @nodes
+    while node
+      count += 1
+      node = node.link
+    end
+    count
   end
+
+  def to_s
+    puts "nodes = #{@nodes}, head = #{@head}, tail = #{@tail}"
+  end
+
 end
 
 
@@ -88,25 +141,69 @@ ll.length            #=> 2
 ll.each {|x| puts x} #=> prints out "first", "third"
 
 class LinkedList
+  attr_accessor :nodes, :head
+  class Node
+    attr_accessor :item, :link
+    def initialize(item, link)
+      @item = item
+      @link = link
+    end
+  end
+
   def initialize
-    # your implementation here
+    @nodes = nil
   end
+
   def empty?
-    # your implementation here
+    @nodes.nil?
   end
+
   def length
-    # your implementation here
+    count = 0
+    node = @nodes
+    while node
+      count += 1
+      node = node.link
+    end
+    count
   end
-  def <<(item)
-    # your implementation here
+
+  def << (item)
+    n = Node.new item, @nodes
+    if self.length == 0         #mark the very first item in the queue, so it can be dequeued
+      @head = n
+    end
+    @nodes = n
   end
+
   def first
-    # your implementation here
+    @nodes.nil? ? nil : @head.item
   end
+
   def last
-    # your implementation here
+    @nodes.nil? ? nil : @nodes.item
   end
+
+  def delete (item_to_delete)
+    node = @nodes
+    while node
+      if node.link.item == item_to_delete
+        #puts "node = #{node}, item_to_delete = #{item_to_delete}"
+        next_node = node.link
+        node.link = next_node.link
+      end
+      node = node.link
+    end
+    item_to_delete
+  end
+
+  def to_s
+    puts "Linked List = #{@nodes}, head = #{@head}, tail = #{@tail}"
+  end
+
   def each(&block)
-    # your implementation here
+    for node in @nodes
+      yield(node)
+    end
   end
 end
