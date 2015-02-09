3b95caea04491696b516ca4217e7c19e9814b606
Chris Peak <christopeak@gmail.com>
2015-02-08 20:50:31 -0800

    begin assignment 5

--- assignments/assignment05.rb
+++ assignments/assignment05.rb
@@ -5,15 +5,34 @@
 # ========================================================================================
 #  Problem 1 -  implement prop_reader, write MiniTest unit tests
 
-# expected results:
-class PropReader
-  prop_reader :flavor
-  def initialize(flavor)
-    @flavor = flavor
+class Class
+  def prop_reader(prop_name)
+    define_method prop_name do
+      instance_variable_get "@#{prop_name}" 
+    end
+    #define_method "#{prop_name}=" {|val| instance_variable_set "@#{prop_name}", val }
   end
 end
 
-obj = PropReader.new "spicy"
-obj.respond_to? :flavor     #=> true
-obj.respond_to? :"flavor="  #=> false
-obj.flavor                  #=> "spicy"
+# expected results:
+class PropReader
+  prop_reader :flavor
+#  def initialize(flavor)
+#    @flavor = flavor
+#  end
+end
+
+
+
+require 'minitest/autorun'
+
+class TestPropReader < Minitest::Test
+  def test_check
+    assert true
+  end
+end
+
+#obj = PropReader.new "spicy"
+#obj.respond_to? :flavor     #=> true
+#obj.respond_to? :"flavor="  #=> false
+#obj.flavor                  #=> "spicy"
