03e5395b28b949f957f1793375085d2626f01631
Chris Peak <christopeak@gmail.com>
2015-02-09 21:22:34 -0800

    completed assignment 5

--- assignments/assignment05.rb
+++ assignments/assignment05.rb
@@ -10,16 +10,15 @@ class Class
     define_method prop_name do
       instance_variable_get "@#{prop_name}" 
     end
-    #define_method "#{prop_name}=" {|val| instance_variable_set "@#{prop_name}", val }
   end
 end
 
 # expected results:
 class PropReader
-  prop_reader :flavor
-#  def initialize(flavor)
-#    @flavor = flavor
-#  end
+  prop_reader(:flavor)
+   def initialize(flavor)
+     @flavor = flavor
+   end
 end
 
 
@@ -28,11 +27,14 @@ require 'minitest/autorun'
 
 class TestPropReader < Minitest::Test
   def test_check
-    assert true
+    pr = PropReader.new("aparameter")
+    assert_equal "aparameter", pr.flavor
+    assert pr.respond_to? :flavor
+    refute pr.respond_to? :"flavor="
   end
 end
 
-#obj = PropReader.new "spicy"
+#obj = PropReader.new("spicy"
 #obj.respond_to? :flavor     #=> true
 #obj.respond_to? :"flavor="  #=> false
 #obj.flavor                  #=> "spicy"
