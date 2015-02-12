7962bf98f5e1952d1c02c9bfa926afeb021b4c41
shawnl11 <shawn.m.lee11@gmail.com>
2015-02-12 13:31:43 -0800

    Update assignment05.rb

--- assignments/assignment05.rb
+++ assignments/assignment05.rb
@@ -4,6 +4,14 @@
 
 # ========================================================================================
 #  Problem 1 -  implement prop_reader, write MiniTest unit tests
+class Class
+  +  def prop_reader(prop_name)
+  +    define_method prop_name do
++      instance_variable_get "@#{prop_name}"
++    end
++  end
+end
+
 
 # expected results:
 class PropReader
@@ -17,3 +25,24 @@ obj = PropReader.new "spicy"
 obj.respond_to? :flavor     #=> true
 obj.respond_to? :"flavor="  #=> false
 obj.flavor                  #=> "spicy"
+
+
+require 'minitest/autorun'
+
+class TestPropReader < Minitest::Test
+  def setup
++   @obj = PropReader.new "spicy"
++ end
++
++ def test_respond_to_flavor
++   assert @obj.respond_to? :flavor
++ end
++
++ def test_not_respond_to_flavor=
++   refute @obj.respond_to? :"flavor="
++ end
++
++ def test_flavor_equals
++   assert_equal @obj.flavor, "spicy"
++ end
++end
