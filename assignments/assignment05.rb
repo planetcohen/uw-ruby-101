840fc323a2e69a2db044a1ab8e0030f4547da6ae
Tim Weisz <tweisz@hotmail.com>
2015-02-13 06:51:49 +0430

    Here is Assignment 5

--- assignments/assignment05.rb
+++ assignments/assignment05.rb
@@ -5,15 +5,36 @@
 # ========================================================================================
 #  Problem 1 -  implement prop_reader, write MiniTest unit tests
 
-# expected results:
+
+class Class
+	def prop_reader(*prop_names)
+		prop_names.each do |prop_name|
+			define_method prop_name do
+				instance_variable_get "@#{prop_name}"
+			end
+		end
+	end
+end
+
 class PropReader
   prop_reader :flavor
   def initialize(flavor)
     @flavor = flavor
   end
+end 
+
+require 'minitest/autorun'
+
+class TestAssignment05 < MiniTest::Test
+
+	def test_propreader
+		obj = PropReader.new "spicy"
+		a = obj.respond_to? :flavor
+		assert_equal true, a
+		b = obj.respond_to? :"flavor="
+		assert_equal false, b
+		assert_equal "spicy", obj.flavor
+	end
+
 end
 
-obj = PropReader.new "spicy"
-obj.respond_to? :flavor     #=> true
-obj.respond_to? :"flavor="  #=> false
-obj.flavor                  #=> "spicy"
