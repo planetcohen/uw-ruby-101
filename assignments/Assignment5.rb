62ee797a9ead4ce4277e6aac7697083caa648c34
Hector Otero <otero111@hotmail.com>
2015-02-12 16:52:54 -0800

    Update Assignment5.rb

--- assignments/Assignment5.rb
+++ assignments/Assignment5.rb
@@ -1,30 +1,30 @@
 require 'minitest/autorun'
 
 class Class
-	def prop_reader(prop_name)
-		define_method(prop_name) do
-			instance_variable_get("@#{prop_name}")
-		end
-	end
+  def prop_reader(prop_name)
+    define_method(prop_name) do
+      instance_variable_get("@#{prop_name}")
+    end
+  end
 end
 
 class PropReader
-	prop_reader(:flavor)
+  prop_reader(:flavor)
 
-	def initialize(flavor)
-		@flavor = flavor
-	end
+  def initialize(flavor)
+    @flavor = flavor
+  end
 end
 
 class PropReaderTest < Minitest::Test
-	def setup
-		@prop_reader = PropReader.new("spicy")
-	end
+  def setup
+    @prop_reader = PropReader.new("spicy")
+  end
 
-	def test_hacker
-		assert @prop_reader.respond_to? :flavor
-		refute @prop_reader.respond_to? :"flavor="
-	end
+  def test_hacker
+    assert @prop_reader.respond_to? :flavor
+    refute @prop_reader.respond_to? :"flavor="
+  end
 end
 
 obj = PropReader.new("spicy")
