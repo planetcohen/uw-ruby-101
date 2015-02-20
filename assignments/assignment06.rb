5e1128d22ef76f950b89829d86ef4afa6470f3d7
Hector Otero <otero111@hotmail.com>
2015-02-19 16:51:27 -0800

    Assignment 6
    Assignment 6

--- assignments/assignment06.rb
+++ assignments/assignment06.rb
@@ -7,26 +7,77 @@
 
 # implement a PriorityQueue, validate using MiniTest unit tests
 
-class PriorityQueue
-  def initialize
-    # your implementation here
-  end
-  def enqueue(item, priority=:medium)
-    # your implementation here
-  end
-  def dequeue
-    # your implementation here
-  end
-  def empty?
-    # your implementation here
-  end
-  def peek
-    # your implementation here
-  end
-  def length
-    # your implementation here
-  end
+require 'minitest/autorun'
+
+class Element
+	#include Comparable
+
+	attr_accessor :name, :priority
+
+	def initialize(name, priority=:medium)
+		if priority == :high
+			priority = 4
+		elsif priority == :medium
+			priority = 3
+		else priority == :low
+			priority = 1
+		end
+		@name, @priority=name, priority
+	end
+
+	def <=>(other)
+		@priority <=> other.priority
+	end
 end
+
+class PriorityQueue
+
+	attr_accessor :elements
+
+	def initialize
+		@elements = []
+	end
+
+	def enqueue(element)#<<(element)
+		@elements<<element
+	end
+
+	def empty?
+		@elements.empty?
+	end
+
+	def dequeue
+		last_element_index = @elements.size - 1
+		@elements.sort!
+		@elements.delete_at(last_element_index)
+	end
+
+	def peek
+		@elements[-1]
+	end
+
+	def length
+		@elements.size
+	end
+end
+
+q = PriorityQueue.new
+q.empty?
+q.enqueue(Element.new("first"))
+q.empty?
+
+q.enqueue(Element.new("top", :high))
+q.enqueue(Element.new("last", :low))
+q.enqueue(Element.new("second"))
+q.enqueue(Element.new("another top", :high))
+
+q.length
+
+q.dequeue
+q.dequeue
+q.dequeue
+q.dequeue
+q.dequeue
 
 # expected results:
 pq = PriorityQueue.new
