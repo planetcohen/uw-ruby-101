576fc52670d633ee83d9fd150a741e66c659c752
Derek Covey <derek.covey@gmail.com>
2015-02-19 17:47:52 -0800

    Assignment06-covey.rb
    Completed HW 6

--- assignments/assignment06.rb
+++ assignments/assignment06.rb
@@ -1,5 +1,5 @@
 # ========================================================================================
-# Assignment 5
+# Assignment 6
 # ========================================================================================
 
 # ========================================================================================
@@ -7,46 +7,110 @@
 
 # implement a PriorityQueue, validate using MiniTest unit tests
 
-class PriorityQueue
+class Priority_Queue
+  attr :list, :nL, :nH, :length
   def initialize
-    # your implementation here
+    @list = []
+    @nL = 0
+    @nH = 0
+    @length = 0
   end
-  def enqueue(item, priority=:medium)
-    # your implementation here
+
+  def enqueue(item, priority = :normal)
+    entry = {item: item, priority: priority}
+    case priority
+    when :high
+      @list.insert(@length - @nH, entry)
+      @nH += 1
+    when :low
+      @list.insert(0, entry)
+      @nL += 1
+    else
+      @list.insert(@nL, entry)
+    end
+    @length += 1
   end
+
   def dequeue
-    # your implementation here
+    if @length > 0
+      last = @list.pop
+      if last[:priority] == :high
+        @nH -= 1
+      elsif last[:priority] == :low
+        @nL -= 1
+      end
+      @length -= 1
+      last[:item]
+    else
+      nil
+    end
   end
+
   def empty?
-    # your implementation here
+    @length == 0
   end
+
   def peek
-    # your implementation here
-  end
-  def length
-    # your implementation here
+    @last
   end
 end
 
-# expected results:
-pq = PriorityQueue.new
-pq.empty?      #=> true
 
-pq.enqueue "first"
-pq.empty?      #=> false
+require 'minitest/autorun'
 
-pq.enqueue "top", :high
-pq.enqueue "last", :low
-pq.enqueue "second"
-pq.enqueue "another top", :high
+class TestPriorityQueue < Minitest::Test
+  def test_create_object
+    pq = Priority_Queue.new
+    assert_equal [], pq.list
+    assert_equal true, pq.empty?
+  end
 
-pq.length     #=> 5
+  def test_enqueue
+    pq = Priority_Queue.new
+    pq.enqueue "first"
+    assert_equal false, pq.empty?
+  end
 
-pq.dequeue    #=> "top"
-pq.dequeue    #=> "another top"
-pq.dequeue    #=> "first"
-pq.dequeue    #=> "second"
-pq.dequeue    #=> "last"
+  def test_priority
+    pq = Priority_Queue.new
+    pq.enqueue "first"
+    pq.enqueue "top", :high
+    pq.enqueue "last", :low
+    pq.enqueue "second"
+    pq.enqueue "another top", :high
+
+    assert_equal 5, pq.length
+
+    assert_equal "top", pq.dequeue
+    assert_equal "another top", pq.dequeue
+    assert_equal "first", pq.dequeue
+    assert_equal "second", pq.dequeue
+    assert_equal "last", pq.dequeue
+  end
+
+
+end
+
+
+# # expected results:
+# pq = PriorityQueue.new
+# pq.empty?      #=> true
+
+# pq.enqueue "first"
+# pq.empty?      #=> false
+
+# pq.enqueue "top", :high
+# pq.enqueue "last", :low
+# pq.enqueue "second"
+# pq.enqueue "another top", :high
+
+# pq.length     #=> 5
+
+# pq.dequeue    #=> "top"
+# pq.dequeue    #=> "another top"
+# pq.dequeue    #=> "first"
+# pq.dequeue    #=> "second"
+# pq.dequeue    #=> "last"
 
 
 # ========================================================================================
@@ -59,11 +123,43 @@ class Recipe
   def initialize(name)
     @name = name
   end
-  
+
   def render_dsl
-    # your code here
+    def render_ingredients
+      list_i = ""
+      @ingredients.each do |i|
+         list_i << "    x \""+i.to_s+"\"\n"
+      end
+      list_i.chomp
+    end
+
+    def render_steps
+      list_s = ""
+      @steps.each do |s|
+         list_s << "    x \""+s.to_s+"\"\n"
+      end
+      list_s.chomp
+    end
+
+    dsl =
+<<RECIPE
+recipe "#{@name}" do
+  category "#{@category}"
+  prep_time "#{@prep_time}"
+  rating #{@rating}
+  ingredients do
+#{render_ingredients}
+  end
+  steps do
+#{render_steps}
   end
 end
+RECIPE
+    # puts dsl
+  end
+end
+
+
 
 class RecipeBuilder
   def recipe(name, &block)
@@ -71,31 +167,31 @@ class RecipeBuilder
     self.instance_eval &block
     @recipe
   end
-  
+
   def category(value)
     @recipe.category = value
   end
-  
+
   def prep_time(value)
     @recipe.prep_time = value
   end
-  
+
   def rating(value)
     @recipe.rating = value
   end
-  
+
   def ingredients(&block)
     @recipe.ingredients = []
     @items = @recipe.ingredients
     self.instance_eval &block
   end
-  
+
   def steps(&block)
     @recipe.steps = []
     @items = @recipe.steps
     self.instance_eval &block
   end
-  
+
   def x(item)
     @items << item
   end
@@ -104,4 +200,44 @@ end
 def recipe(name, &block)
   rb = RecipeBuilder.new
   rb.recipe(name, &block)
+end
+
+
+require 'minitest/autorun'
+
+class TestDSLRender < Minitest::Test
+  def test_dsl_render
+    r = recipe "Bacon" do
+      category "delicious"
+      prep_time "15 min"
+      rating 5
+      ingredients do
+        x "bacon"
+        x "so much bacon"
+      end
+      steps do
+        x "lay down ya bacon"
+        x "sizzle it"
+      end
+    end
+
+    r_compare =
+<<RECIPE
+recipe "Bacon" do
+  category "delicious"
+  prep_time "15 min"
+  rating 5
+  ingredients do
+    x "bacon"
+    x "so much bacon"
+  end
+  steps do
+    x "lay down ya bacon"
+    x "sizzle it"
+  end
+end
+RECIPE
+     assert_equal r.render_dsl, r_compare
+  end
+
 end
