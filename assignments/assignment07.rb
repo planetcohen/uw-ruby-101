f0b8b2e69475d5ab9c6aa7d39238e76d4fc3d7be
Michael Cohen <michael@planetcohen.com>
2015-02-19 19:45:53 -0800

    added slides for lecture 7; added assignment 7

---
+++ assignments/assignment07.rb
@@ -0,0 +1,32 @@
+# ========================================================================================
+# Assignment 7
+# ========================================================================================
+
+# ========================================================================================
+#  Problem 1 - Observable
+
+# implement Observable
+# validate using MiniTest unit tests
+
+module Assignment07
+  module Observable
+    def add_observer(obj)
+      # your implementation here
+    end
+    def delete_observer(obj)
+      # your implementation here
+    end
+    def delete_observers
+      # your implementation here
+    end
+    def changed(new_state=true)
+      # your implementation here
+    end
+    def changed?
+      # your implementation here
+    end
+    def notify_observers(*args)
+      # your implementation here
+    end
+  end
+end
