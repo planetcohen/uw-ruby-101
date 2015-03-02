6ec2b6f3f4df859b555b2e4146e7b0dbe4da8a81
John Smith <puzzle@gmail.com>
2015-03-02 14:50:45 -0800

    Homework 08 complete

--- assignments/assignment07.rb
+++ assignments/assignment07.rb
@@ -11,22 +11,175 @@
 module Assignment07
   module Observable
     def add_observer(obj)
-      # your implementation here
+      if @observers.nil?
+        @observers = []
+      end
+      @observers << obj
     end
     def delete_observer(obj)
-      # your implementation here
+      @observers.delete obj
     end
     def delete_observers
-      # your implementation here
+      @observers.clear
+    end
+    def count_observers
+      @observers.count
     end
     def changed(new_state=true)
-      # your implementation here
+      @changed = new_state
     end
     def changed?
-      # your implementation here
+      if @changed.nil?
+        @changed = false
+      end
+      @changed
     end
     def notify_observers(*args)
-      # your implementation here
+      if changed?
+        @observers.each {|observer| observer.update(*args)}
+        @changed = false
+      end
     end
   end
 end
+
+require 'minitest/autorun'
+
+class ObserveMe
+  include Assignment07::Observable
+
+  def initialize(some_int)
+    @my_value = some_int
+  end
+  
+  def up
+    @my_value += 1
+    changed
+    notify_observers @temp
+  end
+  
+  def down
+    @my_value += 1
+    changed
+    notify_observers @temp
+  end
+  
+end  # class ObserveMe
+
+class Observer
+  def initialize(target)
+    target.add_observer self
+  end
+  
+  def update(some_value)
+    puts some_value
+  end
+end  # class observer
+
+class TestAssignment07 < Minitest::Test
+
+#  def test_queue
+#    q = Assignment04::Queue.new
+#    refute_nil q
+#    assert_empty q            #=> true
+#    assert_equal 0, q.length
+#
+#    assert_equal q, q.enqueue("first")
+#    refute_empty q            #=> true
+#    assert_equal "first", q.peek
+
+#    assert_nil q.dequeue           #=> nil
+#  end
+
+  def test_notify_changed
+    # create observable thing
+    om = ObserveMe.new 68
+    # add observer to thing
+    obr = Observer.new om
+    # change thing's state
+    # notify_observers
+    # verify observer was notified
+    assert_equal 69, om.up
+    # notify_observers
+    # verify observer was not notified
+    assert_nil om.notify_observers
+    # change thing's state
+    om.up
+    # delete observer
+    om.delete_observer(obr)
+    # notify_observers
+    # verify observer was not notified
+    assert_nil om.notify_observers
+  end
+
+  def test_changed_query
+    # create observable thing
+    # verify changed? returns false
+    # add observer to thing
+    # verify changed? returns false
+    # .changed()
+    # verify changed? returns true
+    # .changed(false)
+    # verify changed? returns false
+    # .changed(true)
+    # verify changed? returns true
+    # notify_observers
+    # verify changed? returns false
+    # .changed(false)
+    # verify changed? returns false
+  end
+
+  def test_changed_bad
+    # create observable thing
+    # .changed(foo)
+    # verify no assert :)
+    # verify changed? returns false
+    # .changed()
+    # verify changed? returns true
+    # .changed(foo)
+    # verify no assert :)
+    # verify changed? returns true
+  end
+
+  def test_delete_notify
+    # create observable thing
+    # add 3 observers to thing
+    # change thing's state
+    # notify_observers
+    # verify observers were notified
+    # delete 2nd observer
+    # change thing's state
+    # verify deleted observer is not notified
+    # verify remaining 2 observers are notified
+  end
+
+  def test_count_observers
+    # create observable thing
+    # count_observers - verify count = 0
+    # add 3 observers to thing
+    # count_observers - verify count = 3
+    # delete 2nd observer
+    # count_observers - verify count = 2
+    # delete_observers
+    # count_observers - verify count = 0
+  end
+
+  def test_delete
+    # create observable thing
+    # delete_observer
+    # verify no assert :)
+    # delete_observers
+    # verify no assert :)
+    # add 6 observers
+    # count_observers - verify count = 6
+    # delete_observer
+    # delete_observer
+    # delete_observer
+    # count_observers - verify count = 3
+    # delete_observers
+    # count_observers - verify count = 0
+  end
+
+  # test_notify_observers
+
+end # class TestAssignment07 < Minitest::Test
--- assignments/assignment08.rb
+++ assignments/assignment08.rb
@@ -1,22 +1,22 @@
 # ========================================================================================
 # Assignment 8
 # ========================================================================================
-
+module Assignment08
 # ========================================================================================
 #  Problem 1 - Roman Numerals
 
 # implement conversion between integers and roman numerals
 # validate using MiniTest unit tests
 
-module Assignment08
   class RomanNumeral
+    # Converts integer between 0 & 3999 inclusive to Roman numeral string.
     def initialize(i)
-      @rn_int = i
-      @rn_str = self.to_s
       @ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
       @tens = ["", "X", "XX", "XXX", "LX", "L", "LX", "LXX", "LXXX", "XC"]
       @hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
       @thousands = ["", "M", "MM", "MMM"]
+      @rn_int = i
+      @rn_str = self.to_s
     end
 
     def to_s
@@ -29,7 +29,7 @@ module Assignment08
         while digit_string.length < 4
           digit_string = "0" + digit_string
         end
-        digits = digit_string.map {|str_digit| digit.to_i}
+        digits = digit_string.split("").map {|str_digit| str_digit.to_i}
         @thousands[digits[0]] + @hundreds[digits[1]] + @tens[digits[2]] + @ones[digits[3]]
       end
     end
@@ -45,7 +45,7 @@ module Assignment08
     end
 
   end  # class RomanNumeral
-end  # module Assignment08
+
 
 # expected results:
 RomanNumeral.new(1).to_s   # => 'I'
@@ -61,13 +61,12 @@ RomanNumeral.new(32).to_s  # => 'XXXII'
 RomanNumeral.new(51).to_s  # => 'LI'
 
 # bonus:
-RomanNumeral.from_string('III').to_i  # => 3
-RomanNumeral.from_string('IX').to_i   # => 9
-RomanNumeral.from_string('IX').to_i   # => 9
+# Assignment08::RomanNumeral.from_string('III').to_i  # => 3
+# Assignment08::RomanNumeral.from_string('IX').to_i   # => 9
+# Assignment08::RomanNumeral.from_string('IX').to_i   # => 9
 
 # given any arbitrary integer n:
-n == RomanNumeral.from_string(RomanNumeral.new(n).to_s).to_i
-
+# n == Assignment08::RomanNumeral.from_string(Assignment08::RomanNumeral.new(n).to_s).to_i
 
 
 # ========================================================================================
@@ -77,13 +76,82 @@ n == RomanNumeral.from_string(RomanNumeral.new(n).to_s).to_i
 # calculate the golden ratio up to specified precision
 # validate using MiniTest unit tests
 
-module Assignment08
-  def golden_ratio(precision)
-    # your implementation here
+  def fib(n)
+    def calc_fib(n, cache)
+      if cache.length > n
+        cache[n]
+      else
+        cache[n] = calc_fib(n-1, cache) + calc_fib(n-2, cache)
+      end
+    end
+    calc_fib(n, [1,1])
   end
-end
+  def golden_ratio_fib(count)
+    fib(count) / fib(count - 1).to_f
+  end
+  def golden_ratio(precision)
+    case precision
+    when 1
+      fib = 5
+    when 2
+      fib = 8
+    when 3
+      fib = 10
+    when 4
+      fib = 12
+    when 5
+      fib = 13
+    when 6
+      fib = 16
+    when 7
+      fib = 19
+    when 8
+      fib = 22
+    when 9
+      fib = 24
+    when 10
+      fib = 27
+    end
+    golden_ratio_fib(fib).round(precision)
+  end
 
 # expected results:
-golden_ratio(2)  # => 1.62
-golden_ratio(5)  # => 1.61803
-golden_ratio(8)  # => 1.61803399
+# golden_ratio(2)  # => 1.62 
+# golden_ratio(5)  # => 1.61803 
+# golden_ratio(8)  # => 1.61803399 
+
+# GR = fib(23) / fib(22).to_f
+
+end  # module Assignment08
+
+require 'minitest/autorun'
+
+class TestAssignment08 < Minitest::Test
+  include Assignment08
+  def test_golden_ratio
+    assert_equal golden_ratio(1), 1.6
+    assert_equal golden_ratio(2), 1.62
+    assert_equal golden_ratio(3), 1.618
+    assert_equal golden_ratio(4), 1.6181
+    assert_equal golden_ratio(5), 1.61803
+    assert_equal golden_ratio(6), 1.618034
+    assert_equal golden_ratio(7), 1.6180340
+    assert_equal golden_ratio(8), 1.61803399
+    assert_equal golden_ratio(9), 1.618033989
+    assert_equal golden_ratio(10), 1.6180339887
+  end
+  
+  def test_roman_numeral
+    assert_equal RomanNumeral.new(1).to_s, 'I'
+    assert_equal RomanNumeral.new(2).to_s, 'II'
+    assert_equal RomanNumeral.new(3).to_s, 'III'
+    assert_equal RomanNumeral.new(4).to_s, 'IV'
+    assert_equal RomanNumeral.new(5).to_s, 'V'
+    assert_equal RomanNumeral.new(6).to_s, 'VI'
+    assert_equal RomanNumeral.new(9).to_s, 'IX'
+    assert_equal RomanNumeral.new(10).to_s, 'X'
+    assert_equal RomanNumeral.new(19).to_s, 'XIX'
+    assert_equal RomanNumeral.new(32).to_s, 'XXXII'
+    assert_equal RomanNumeral.new(51).to_s, 'LI'
+  end
+end  # TestAssignment08 < Minitest::Test
