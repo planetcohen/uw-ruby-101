238b78ec9fa43bce72a43be00ebaa139fd9ee817
Derek Covey <derek.covey@gmail.com>
2015-03-03 21:26:29 -0800

    HW08 P1 & P2 complete

--- assignments/assignment08.rb
+++ assignments/assignment08.rb
@@ -47,28 +47,34 @@ module Assignment08
     end
 
     def to_i
-      tens = @roman1
-      last_ten = tens.pop
-      #roman =~ /#{roman1[c]}/ || @roman =~ /#{@roman5[c]}/
-      result = tens.map.with_index do |c, i|
-        if @roman =~ /#{@roman1[i]}#{@roman1[i+1]}/
-          9
-        elsif @roman =~ /#{@roman1[i]}#{@roman5[i]}/
-          4
-        elsif @roman =~ /#{@roman5[i]}/
-          if @roman =~ /#{@roman1[i]}/
-            5 + @roman[/#{@roman1[i]}+/].length
+      #The following code is not very pretty.  I'd like to go back
+      #and clean this up
+      convert = @roman
+      last_ten = @roman1.last
+      result = @roman1.map.with_index do |c, i|
+        unless i == @roman1.length - 1
+          if convert =~ /#{@roman1[i]}#{@roman1[i+1]}/
+            match = convert[/#{@roman1[i]}#{@roman1[i+1]}/]
+            convert = convert.sub(/#{match}/, '')
+            9
+          elsif convert =~ /#{@roman1[i]}#{@roman5[i]}/
+            4
+          elsif convert =~ /#{@roman5[i]}/
+            if convert =~ /#{@roman1[i]}/
+              5 + convert[/#{@roman1[i]}+/].length
+            else
+              5
+            end
+          elsif convert =~ /#{@roman1[i]}/
+              convert[/#{@roman1[i]}+/].length
           else
-            5
+            0
           end
-        elsif @roman =~ /#{@roman1[i]}/
-            @roman[/#{@roman1[i]}+/].length
         else
-          0
+          if convert =~ /#{@roman1[i]}+/
+            convert[/#{@roman1[i]}+/].length
+          end
         end
-      end
-      if @roman =~ /#{last_ten}+/
-        result << @roman[/#{last_ten}+/].length
       end
       result.reverse.join.to_i
     end
@@ -80,27 +86,14 @@ module Assignment08
     end
   end
 
-  def write_to_file(title, &block)
-    File.open(title, "w+") do |output|
-      output.write block.call
-    end
-  end
-
-  def calc_test_sequence(test_greek)
-    write_to_file "calc_test_sequence.txt" do
-      test_out = test_greek.map do |g|
-        <<-NUM
-        #{(RomanNumeral.new g).to_s}
-        NUM
-      end
-      test_out.join
-    end
-  end
 
 
   require 'minitest/autorun'
 
   class TestRoman < Minitest::Test
+    #Just wanted to practice reading/writing to a file, so added some gratuitous
+    #I/O.  Kind of handy for looking at the Roman numerals that are produced
+
     def write_to_file(title, &block)
       File.open(title, "w+") do |output|
         output.write block.call
@@ -129,36 +122,35 @@ module Assignment08
 
         test_greek.map.with_index do |g, i|
           assert_equal (RomanNumeral.new g).to_s, test_roman[i].chomp
-          #temp = RomanNumeral.new g
-          #temp.to_s
-          #assert_equal g, temp.to_i
+          temp = RomanNumeral.new g
+          temp.to_s
+          assert_equal g, temp.to_i
         end
       end
     end
   end
-
 end
 
-# expected results:
-RomanNumeral.new(1).to_s   # => 'I'
-RomanNumeral.new(2).to_s   # => 'II'
-RomanNumeral.new(3).to_s   # => 'III'
-RomanNumeral.new(4).to_s   # => 'IV'
-RomanNumeral.new(5).to_s   # => 'V'
-RomanNumeral.new(6).to_s   # => 'VI'
-RomanNumeral.new(9).to_s   # => 'IX'
-RomanNumeral.new(10).to_s  # => 'X'
-RomanNumeral.new(19).to_s  # => 'XIX'
-RomanNumeral.new(32).to_s  # => 'XXXII'
-RomanNumeral.new(51).to_s  # => 'LI'
-
-# bonus:
-RomanNumeral.from_string('III').to_i  # => 3
-RomanNumeral.from_string('IX').to_i   # => 9
-RomanNumeral.from_string('IX').to_i   # => 9
-
-# given any arbitrary integer n:
-n == RomanNumeral.from_string(RomanNumeral.new(n).to_s).to_i
+# # expected results:
+# RomanNumeral.new(1).to_s   # => 'I'
+# RomanNumeral.new(2).to_s   # => 'II'
+# RomanNumeral.new(3).to_s   # => 'III'
+# RomanNumeral.new(4).to_s   # => 'IV'
+# RomanNumeral.new(5).to_s   # => 'V'
+# RomanNumeral.new(6).to_s   # => 'VI'
+# RomanNumeral.new(9).to_s   # => 'IX'
+# RomanNumeral.new(10).to_s  # => 'X'
+# RomanNumeral.new(19).to_s  # => 'XIX'
+# RomanNumeral.new(32).to_s  # => 'XXXII'
+# RomanNumeral.new(51).to_s  # => 'LI'
+#
+# # bonus:
+# RomanNumeral.from_string('III').to_i  # => 3
+# RomanNumeral.from_string('IX').to_i   # => 9
+# RomanNumeral.from_string('IX').to_i   # => 9
+#
+# # given any arbitrary integer n:
+# n == RomanNumeral.from_string(RomanNumeral.new(n).to_s).to_i
 
 
 
