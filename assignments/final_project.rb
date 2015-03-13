6914280faa9d8fa691fa166190a77ae09a977cb6
Michelle Yu <michellehyu7@gmail.com>
2015-03-12 18:48:42 -0700

    final project conway's game of life
    Signed-off-by: Michelle Yu <michellehyu7@gmail.com>

--- assignments/final_project.rb
+++ assignments/final_project.rb
@@ -56,20 +56,125 @@
 
 #  ---------------------------------------------------------------------------------------
 #  Suggested Implementation
+require 'minitest/autorun'
 
 module FinalProject
   class GameOfLife
+    attr :cells
     def initialize(size)
       # randomly initialize the board
+      @size = size
+      @cells = Array.new(@size){Array.new(@size){Random.rand(@size)%2}}
     end
+
+    def cells=(new_cells)
+      @cells = new_cells
+    end
+
     def evolve
+      new_cells = Array.new(@size){Array.new(@size)}
       # apply rules to each cell and generate new state
+      @cells.each_with_index do |row, x|
+        row.each_with_index do |cell, y|
+          neighbors = living_neighbors(x,y)
+          new_cells[x][y] = @cells[x][y]
+          if @cells[x][y] == 1 then
+            if (neighbors < 2 || neighbors > 3) then
+              new_cells[x][y] = 0
+            end
+          else
+            if neighbors == 3 then
+              new_cells[x][y] = 1
+            end
+          end
+        end
+      end
+      @cells = new_cells
     end
+  
+    def living_neighbors(x,y)
+      [[-1,1], [0,1], [1,1],
+       [-1,0], [1, 0],
+       [-1,-1], [0, -1], [1, -1]
+      ].inject(0) do |alive, offset|
+        nx = (x + offset[0].to_i)
+        ny = (y + offset[1].to_i) 
+
+        nx = nx < 0 ? nx+@size : nx % @size
+        ny = ny < 0 ? ny+@size : ny % @size
+      
+        alive = alive + @cells[nx][ny].to_i
+      end
+    end
+
     def render
       # render the current state of the board
+      print "- - " * @size
+      print "\n"
+      
+      @cells.map do |row| 
+        print "|"; 
+        print row.map{|cells| cells==1 ? 'o ' : '  '}.join(" "); 
+        print "|\n"
+      end
+      
+      print "- - " * @size
+      print "\n"
     end
+
     def run(num_generations)
       # evolve and render for num_generations
+      num_generations.times do |i| 
+        system "clear"
+        print "Generation ", i+1, "\n"
+        evolve
+        render
+        sleep 1
+      end
     end
   end
+  
+  gol = GameOfLife.new(10)
+  puts "initial board"
+  gol.render
+  gol.run(10)
+end
+
+class TestMyClass < Minitest::Test
+  include FinalProject
+
+  def test_living_neighbors
+   gol = GameOfLife.new(3)
+   #test no neighbor
+   gol.cells = [[0,0,0], [0,1,0], [0,0,0]]
+   assert_equal 0, gol.living_neighbors(1,1)
+   #test 8 neighbors
+   gol.cells = [[1,1,1], [1,1,1], [1,1,1]]
+   assert_equal 8, gol.living_neighbors(1,1)
+   #test border neighbor
+   gol.cells = [[0,0,0], [0,1,0], [1,0,0]]
+   assert_equal 2, gol.living_neighbors(0,0)
+  end
+
+  def test_evolve
+   
+   gol = GameOfLife.new(3)
+   #test 3 neighbor become alive
+   gol.cells = [[0,1,0], [1,0,1], [0,0,0]]
+   assert_equal 0, gol.cells[1][1]
+   gol.evolve
+   assert_equal 1, gol.cells[1][1]
+   #test alive 0 neighbor dies
+   gol.cells = [[0,0,0], [0,1,0], [0,0,0]]
+   assert_equal 1, gol.cells[1][1]
+   gol.evolve
+   assert_equal 0, gol.cells[1][1]
+   #test alive 3 neighbor stay alive
+   gol.cells = [[1,0,0], [1,1,1], [0,0,0]]
+   assert_equal 1, gol.cells[0][0]
+   gol.evolve
+   assert_equal 1, gol.cells[0][0]
+  
+  end
+
 end
